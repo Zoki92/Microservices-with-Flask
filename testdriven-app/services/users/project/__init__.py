9282dba3b23c02be953a2a9b2abc9bd4dab4d49f
Zoran <zoranstoilov@yahoo.com>
2020-07-13 12:35:33 +0100

    Added db setup, users model, sqlalchemy support and tests for config and users

--- testdriven-app/services/users/project/__init__.py
+++ testdriven-app/services/users/project/__init__.py
@@ -1,13 +1,30 @@
+import sys
+import os
+import datetime
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv("APP_SETTINGS")
+app.config.from_object(app_settings)
+
+db = SQLAlchemy(app)
 
 
-@app.route('/users/ping', methods=['GET'])
+@app.route("/users/ping", methods=["GET"])
 def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong'
-    })
+    return jsonify({"status": "success", "message": "pong!"})
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
