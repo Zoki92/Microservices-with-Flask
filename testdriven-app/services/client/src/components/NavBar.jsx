0887819ce1d321a41156413c34a1bd33e9ba190d
Zoran <zoranstoilov@yahoo.com>
2020-08-01 22:36:49 +0100

    Added form component and tests

--- testdriven-app/services/client/src/components/NavBar.jsx
+++ testdriven-app/services/client/src/components/NavBar.jsx
@@ -1,38 +1,37 @@
 import React from "react";
-import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { Navbar, Nav, NavItem, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+
 const NavBar = (props) => (
-  <Navbar inverse collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <span>{props.title}</span>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <LinkContainer to="/">
-          <NavItem eventKey={1}>Home</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/about">
-          <NavItem eventKey={2}>About</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/status">
-          <NavItem eventKey={3}>User Status</NavItem>
-        </LinkContainer>
-      </Nav>
-      <Nav pullRight>
-        <LinkContainer to="/register">
-          <NavItem eventKey={1}>Register</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/login">
-          <NavItem eventKey={2}>Log In</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/logout">
-          <NavItem eventKey={3}>Log Out</NavItem>
-        </LinkContainer>
-      </Nav>
-    </Navbar.Collapse>
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">{props.title}</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer to="/">
+            <NavItem eventKey={1}>Home</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/about">
+            <NavItem eventKey={2}>About</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/status">
+            <NavItem eventKey={3}>User Status</NavItem>
+          </LinkContainer>
+        </Nav>
+        <Nav>
+          <LinkContainer to="/register">
+            <NavItem eventKey={1}>Register</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/login">
+            <NavItem eventKey={2}>Log In</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/logout">
+            <NavItem eventKey={3}>Log Out</NavItem>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
   </Navbar>
 );
 
